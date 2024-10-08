3ade5fd571e7be76866d73ed5eb03d348aa2f21f
Storm <storm.minecrafter.sss@gmail.com>
2024-10-07 21:27:53 -0400

    changed a few variable names

--- obfuscated_code/matrix_inverse_without_libraries.py
+++ obfuscated_code/matrix_inverse_without_libraries.py
@@ -1,34 +1,39 @@
-def exy(m,x,y):
-    return [l[:x]+l[x+1:] for l in m[:y]+m[y+1:]]
-#-25 is goal for det
+"""
+Hello! The me of 2 years ago created this and am amazed at how obfuscated the code is
+I did it because I wanted to challenge myself to see how to take inverse matrices of shapes larger then (2,2)
+"""
+
+def exy(matrix,x,y):
+    return [element[:x]+element[x+1:] for element in matrix[:y]+matrix[y+1:]]
+#-25 is goal for determinant
 
 
-def det(m):
+def determinant(matrix):
     s=0
-    for x in range(len(m[0])):
-        em=exy(m,x,0)
-        if len(m[0])>2:
-            val=det(em)
+    for x in range(len(matrix[0])):
+        em=exy(matrix,x,0)
+        if len(matrix[0])>2:
+            val=determinant(em)
         else:
             val=em[0][0]
-        s+=m[0][x]*val*(2*(x%2==0)-1)
+        s+=matrix[0][x]*val*(2*(x%2==0)-1)
     return s
-#print(det(mat))
-#print(np.linalg.det(mat))
+#print(determinant(mat))
+#print(np.linalg.determinant(mat))
 
-def adj(m):
-    return [[det(exy(m,y,x))*(2*((x+y)%2==0)-1) for x in range(len(m[0]))] for y in range(len(m))]
+def adjoint(matrix):
+    return [[determinant(exy(matrix,y,x))*(2*((x+y)%2==0)-1) for x in range(len(matrix[0]))] for y in range(len(matrix))]
     
-def inv(m):
-    return [[x/det(m) for x in y] for y in adj(m)]
+def inverse(matrix):
+    return [[x/determinant(matrix) for x in y] for y in adjoint(matrix)]
 
-def dot(m1,m2):
+def dot(matrix_1,matrix_2):
     bl=[]
-    for x1 in range(len(m1)):
+    for x1 in range(len(matrix_1)):
         sl=[]
-        for y1 in range(len(m2[0])):
+        for y1 in range(len(matrix_2[0])):
             s=0
-            for x,y in list(zip(m1[x1],[y[y1] for y in m2])):
+            for x,y in list(zip(matrix_1[x1],[y[y1] for y in matrix_2])):
                 s+=x*y
             sl.append(round(s,5))
         bl.append(sl)
@@ -37,10 +42,13 @@ def dot(m1,m2):
 def main():
     import numpy as np
     npmat=np.random.randint(-5,5,(4,4))
-    mat=[[float(x) for x in y] for y in npmat]
-    for x in inv(mat):
-        print(x)
+    matrix=[[float(x) for x in y] for y in npmat]
+    inverse_matrix=inverse(matrix)
     print(np.linalg.inv(npmat))
+    for x in inverse_matrix:
+        print(x)
+    for x in dot(matrix,inverse_matrix):
+        print(x)
 
 if __name__=="__main__":
     main()
